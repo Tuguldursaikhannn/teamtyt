e02cdb66db6ee2adb9a6e3335a388b5c563a5be2
Tuguldursaikhannn <45116765+Tuguldursaikhannn@users.noreply.github.com>
2018-11-17 15:22:25 +0800

    Create TwoDimShape.java

---
+++ oop-lab-7/TwoDimShape.java
@@ -0,0 +1,7 @@
+public class TwoDimShape extends shape {
+   public twoDimShape(){}
+   public twoDimShape(float x, float y, float a, String name){
+       public shape(x, y, a, name);
+   }
+   public float area() = 0;
+}
